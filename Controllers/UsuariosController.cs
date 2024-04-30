56920901abdeac1fb9ee299f5b52590f0258f62f
Luis Morales <cuentaroberto37@gmail.com>
2024-04-30 13:39:29 -0600

    Agregado el primer controlador, de usuario

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaTecnicaIOON.Models;
+
+[ApiController]
+[Route("[controller]")]
+public class UsuariosController : ControllerBase
+{
+    private readonly DbManager _dbManager;
+
+    public UsuariosController(DbManager dbManager)
+    {
+        _dbManager = dbManager;
+    }
+
+    [HttpPost]
+    public IActionResult RegisterUser(Usuarios usuarios)
+    {
+        // Aquí puedes validar los datos de entrada
+
+        _dbManager.InsertUser(usuarios);
+
+        return Ok("registrado con exito...");
+    }
+}
---
+++ DbManager.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using PruebaTecnicaIOON.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+public class DbManager
+{
+    private readonly IConfiguration _config;
+
+    public DbManager(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    private IDbConnection CreateConnection()
+    {
+        string connectionString = _config.GetConnectionString("DefaultConnection");
+        return new SqlConnection(connectionString);
+    }
+
+    public void InsertUser(Usuarios usuarios)
+    {
+        using (IDbConnection dbConnection = CreateConnection())
+        {
+            string query = "INSERT INTO Usuarios (Nombre, Email, Pass) VALUES (@Nombre, @Email, @Pass)";
+            dbConnection.Execute(query, usuarios);
+        }
+    }
+
+    public void InsertBusiness(Negocios negocios)
+    {
+        using (IDbConnection dbConnection = CreateConnection())
+        {
+            string query = "INSERT INTO Negocios (Nombre, Direccion, RUC) VALUES (@Nombre, @Direccion, @RUC)";
+            dbConnection.Execute(query, negocios);
+        }
+    }
+}

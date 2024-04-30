befd9de4bf57c3697a037a62bfe17bc23f59021b
Luis Morales <cuentaroberto37@gmail.com>
2024-04-30 13:24:14 -0600

    Primer commit

---
+++ Models/Negocios.cs
@@ -0,0 +1,11 @@
+﻿namespace PruebaTecnicaIOON.Models
+{
+    public class Negocios
+    {
+        public int NegociosId { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string RUC { get; set; }
+        public Usuarios Usuarios { get; set; }
+    }
+}
---
+++ Models/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaTecnicaIOON.Models
+{
+    public class Usuarios
+    {
+        public int UsuarioId { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+        [Required]
+        public string  Pass { get; set; }
+        public Negocios Negocios { get; set; }
+
+    }
+}
